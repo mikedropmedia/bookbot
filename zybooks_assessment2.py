38309bf6883845dda78f3129956accfa2e8e0ccf
mdMedia <mikedropmedia@gmail.com>
2024-08-28 17:24:14 -0400

    commented out

--- zybooks_assessment2.py
+++ zybooks_assessment2.py
@@ -24,11 +24,11 @@ then the expected output is
 Distance: 197.33 miles
 
 '''
-#Employee A: 15.62 miles
-#Employee B: 41.85 miles
-#Employee C: 32.67 miles
-#solution accepts three integer inputs representing the number of times an employee travels to the job site
-#solution outputs "Distance: " followed by the total value to two decimal places
+# Employee A: 15.62 miles
+# Employee B: 41.85 miles
+# Employee C: 32.67 miles
+# solution accepts three integer inputs representing the number of times an employee travels to the job site
+# solution outputs "Distance: " followed by the total value to two decimal places
 
 trips_a = int(input())
 trips_b = int(input())
